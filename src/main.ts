94d663e39903126b49fc8ec4e98006d471c5e92d
gonzjv <gonzjv@ya.ru>
2023-02-08 09:02:48 +0300

    build: vue router

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import './style.css';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import VueCookies from 'vue-cookies';
+import {
+  createRouter,
+  createWebHistory,
+} from 'vue-router';
+import routes from '~pages';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 const app = createApp(App);
 
